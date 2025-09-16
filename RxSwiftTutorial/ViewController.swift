904356b8bef77683e6d31a8db5553fc581b978aa
bhooncoding <bhooncoding@gmail.com>
2025-09-16 22:11:18 +0900

    update: 검색 tutorial
    combineLatest, TableView.rx.items, throttle

--- RxSwiftTutorial/ViewController.swift
+++ RxSwiftTutorial/ViewController.swift
@@ -14,47 +14,63 @@ import SnapKit
 
 class ViewController: UIViewController {
     
-    // 4. DisposeBag: 구독을 관리할 가방을 만듭니다.
     let disposeBag = DisposeBag()
     
-    let label = UILabel()
-    let button = UIButton(type: .system)
+    let allNames = BehaviorRelay<[String]>(value: [
+        "김철수", "이영희", "박민준", "최지우", "정수빈",
+        "강현우", "조은지", "윤도현", "임서연", "한지훈",
+        "안유진", "오세훈", "배현진", "신짱구", "유리"
+    ])
     
+    // UI
+    let searchBar: UISearchBar = .init()
+    let tableView: UITableView = .init()
+    
+    // LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        setupUI()
-        
-        // 1. Observable: 버튼 탭 이벤트를 감지하는 Observable
-        let buttonTapObservable = self.button.rx.tap
-        
-        buttonTapObservable
-        // 2. Operator: 탭 이벤트를 "버튼 클릭!"이라는 텍스트로 변환(map)합니다.
-            .map { "버튼 클릭!" }
-        // 3. Subscriber: 변환된 텍스트를 받아서 레이블에 바인딩(구독)합니다.
-            .bind(to: label.rx.text)
-        // 4. DisposeBag: 구독이 끝났을 때 메모리에서 해제되도록 disposeBag에 추가합니다.
-            .disposed(by: disposeBag)
+        self.setupUI()
+        self.bind()
     }
     
-    func setupUI() {
-        view.backgroundColor = .white
+    private func setupUI() {
+        self.view.backgroundColor = .white
         
-        self.label.text = "버튼을 눌러주세요."
-        label.textAlignment = .center
+        self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
         
-        button.setTitle("클릭", for: .normal)
+        self.view.addSubview(self.searchBar)
+        self.view.addSubview(self.tableView)
         
-        view.addSubview(label)
-        view.addSubview(button)
-        
-        label.snp.makeConstraints { make in
-            make.center.equalToSuperview()
+        self.searchBar.snp.makeConstraints {
+            $0.top.equalTo(self.view.safeAreaLayoutGuide)
+            $0.leading.trailing.equalToSuperview()
         }
         
-        button.snp.makeConstraints { make in
-            make.top.equalTo(label.snp.bottom).offset(20)
-            make.centerX.equalToSuperview()
+        self.tableView.snp.makeConstraints {
+            $0.top.equalTo(self.searchBar.snp.bottom)
+            $0.leading.trailing.bottom.equalToSuperview()
         }
+    }
+    
+    private func bind() {
+        let searchTextObservable: Observable = self.searchBar.rx.text.orEmpty
+            .throttle(.milliseconds(300), scheduler: MainScheduler.instance)
+            .distinctUntilChanged()
+        
+        let filteredNamesObservable: Observable = .combineLatest(
+            self.allNames,
+            searchTextObservable) { allNames, searchText -> [String] in
+                if searchText.isEmpty {
+                    return allNames
+                }
+                return allNames.filter { $0.contains(searchText) }
+            }
+        
+        filteredNamesObservable
+            .bind(to: self.tableView.rx.items(cellIdentifier: "cell", cellType: UITableViewCell.self)) { row, name, cell in
+                cell.textLabel?.text = name
+            }
+            .disposed(by: self.disposeBag)
     }
 }
